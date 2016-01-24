2f778b6aeffb5f7c2f2a613d09c126a7bcbdb0da
James Lin <james97lin@gmail.com>
2016-01-24 11:59:50 -0800

    Edit

--- Server.py
+++ Server.py
@@ -135,6 +135,6 @@ def disconnect():
     debug(session['id'] + " disconnected.")
 
 if __name__ == '__main__':
-    print("HELLO WORLD!")
+    debug("HELLO WORLD!")
     port = int(os.environ.get("PORT", 5000))
     socketio.run(app, host='0.0.0.0', port=port)
